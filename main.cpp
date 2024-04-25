178c0adedf35465504132f30fa151e5ea5f941c7
siuslawcarrie <102423932+siuslawcarrie@users.noreply.github.com>
2024-04-25 10:32:52 -0700

    working (see note)
    needs new line between accounts

--- main.cpp
+++ main.cpp
@@ -9,57 +9,76 @@
 
 using namespace std;
 
-int main() {
-//   fstream newfile;
-//   newfile.open("tpoint.txt",ios::out);  // open a file to perform write operation using file object
-//   if(newfile.is_open()) //checking whether the file is open {
-//      newfile<<"Tutorials point \n"; //inserting text
-//      newfile.close(); //close the file object
-//   }
-//   newfile.open("tpoint.txt",ios::in); //open a file to perform read operation using file object
-//   if (newfile.is_open()){ //checking whether the file is open
-//      string tp;
-//      while(getline(newfile, tp)){ //read data from file object and put it into string.
-//         cout << tp << "\n"; //print the data of the string
-//      }
-//      newfile.close(); //close the file object.
-//   }
-//}
-    int account_number;
-    char account_type;
-    float minimum_balance, account_balance, level_1_interest = .03, level_2_interest = .05;
-    float savings_interest = .04;
-    int savings_fee = 10, checking_fee = 25;
+
+    int main() {
+
+        int account_number =0;
+        char account_type = ' ';
+        float minimum_balance, account_balance = 0.00;
+        //input file variables & open input file
+        ifstream in;
+        in.open("C:\\Users\\siusl\\CLionProjects\\CS221_Spring_Bank_Accounts\\bank_accounts.txt");
+        if (in.good()) {
+            while (in) {
+                in >> account_number >> account_type >> minimum_balance >> account_balance;
+
+
+                float level_1_interest = .05, level_2_interest = .03;
+                float savings_interest = .04;
+                int savings_fee = 10, checking_fee = 25;
 
 //TESTING ***DELETE***
-    `account_type = 'S';
-    account_balance = 13000;
-    minimum_balance = 4000;
+//    account_type = 'S';
+//    account_balance = 5000;
+//    minimum_balance = 4000;
 //account type checking
-    if (account_type = 'C') {
-        if (account_balance - minimum_balance > 5000){
-            account_balance = account_balance * level_2_interest + account_balance;
-        cout << "Your account is eligible for " << level_2_interest * 100 << "% interest. Your new balance is: $"
-             << account_balance;
-        }
-        else if (account_balance - minimum_balance >= 1)
-        {
-            account_balance = account_balance * level_1_interest + account_balance;
-            cout << "Your account is eligible for " << level_1_interest * 100 << "% interest. Your new balance is: $"
-                 << account_balance;
+                if (account_type = 'C') {
+                    //more than 5000 above minimum balance is 5% interest
+                    if (account_balance - minimum_balance > 5000) {
+                        account_balance = account_balance * level_1_interest + account_balance;
+                        cout << "Account type " << account_type << " account # " << account_number
+                             << " is eligible for "
+                             << level_1_interest * 100 << "% interest. Your new balance is $" <<
+                             account_balance;
+                    }
+                        //less than 5000 over minimum balance is 3% interest
+                    else if (account_balance - minimum_balance >= 1) {
+                        account_balance = account_balance * level_2_interest + account_balance;
+                        cout << "Account type " << account_type << " account # " << account_number
+                             << " is eligible for "
+                             << level_2_interest * 100 << "% interest. Your new balance is $" <<
+                             account_balance;
+                    }
+                        //below minimum balance is a $25 fee
+                    else {
+                        account_balance = account_balance - checking_fee;
+                        cout << "Account type " << account_type << " account # " << account_number
+                             << " does not meet the minimum balance, therefore is subject to a $" <<
+                             checking_fee << " fee. Your new balance is $" << account_balance;
+                    }
+                }
+//savings account
+                else {
+                    //if above minimum balance, 4% interest
+                    if (account_balance - minimum_balance >= 1) {
+                        account_balance = account_balance * savings_interest + account_balance;
+                        cout << "Account type " << account_type << " account # " << account_number
+                             << " is eligible for "
+                             << savings_interest * 100 << "% interest. Your new balance is $" <<
+                             account_balance;
+                    } else {
+                        account_balance = account_balance - savings_fee;
+                        cout << "Account type " << account_type << " account # " << account_number
+                             << " does not meet the minimum balance, therefore is subject to a $" <<
+                             savings_fee << " fee. Your new balance is $" << account_balance;
+                    }
+
+                }
+            }
         }
         else {
-        account_balance = account_balance - checking_fee;
-        cout<<"Your account does not meet the minimum balance and is subject to a $"<<checking_fee<<" fee. Your new balance is $"<<account_balance;
-    }
+            cout << "Could not read file";
+            return -1;
         }
-    if (account_type = 'S'){
-            if (account_balance - minimum_balance >=1){
-                account_balance = account_balance * savings_interest + account_balance;
-                cout <<"Account type " <<account_type<<" account # "<<account_number<<" is eligible for "<<savings_interest * 100<<"% interest. Your new balance is $"<<account_balance;
-            }
-
-    }
-
-      return 0;
+    return 0;
 }
